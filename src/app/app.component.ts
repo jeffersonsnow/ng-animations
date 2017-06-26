86628f1e854324bca33ff2bcb97d0ca27083c8f0
Jefferson Snow <jeffersonsnow@Jeffersons-MacBook-Pro.local>
2017-06-26 09:11:03 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { clickedStateTrigger, numberEnteredStateTrigger } from "app/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    clickedStateTrigger,
+    numberEnteredStateTrigger,
+  ] 
 })
 export class AppComponent {
-  title = 'app works!';
+  clickInfo = 'default';
+  paragraphClick = 'default';
+  numberEntered;
+
+  onClickSimple(){
+    this.clickInfo = 'clicked';
+    setTimeout(()=>{
+      this.clickInfo = 'default';
+    }, 3000);
+  }
 }
